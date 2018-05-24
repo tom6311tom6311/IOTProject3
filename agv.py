0fc01b9119cbe525c860371678550fc1d5ca291f
tomHuang <tom.huang@positivegrid.com>
2018-05-24 16:18:55 +0800

    fix: read serial error
    Signed-off-by: tomHuang <tom.huang@positivegrid.com>

--- agv.py
+++ agv.py
@@ -9,12 +9,12 @@ ultra_sound_dist = 300
 motor_speed = [0, 0]
 
 while 1:
-  raw_input = ser.readline()
+  raw_input = ser.readline().replace('\r', '').replace('\n', '')
   if (raw_input):
-    ultra_sound_dist = int(raw_input)
+    ultra_sound_dist = float(raw_input)
     print(ultra_sound_dist)
   motor_speed[0] = (motor_speed[0] + 1) % 255
-  motor_speed[0] = (motor_speed[0] + 1) % 255
-  ser.write(''.join([str(speed).zfill(4) for speed in motor_speed]))
-  print('Sent: ' + ''.join([str(speed).zfill(4) for speed in motor_speed]))
+  motor_speed[1] = (motor_speed[1] + 1) % 255
+  ser.write(''.join([str(speed).zfill(3) for speed in motor_speed]))
+  print('Sent: ' + ''.join([str(speed).zfill(3) for speed in motor_speed]))
   # time.sleep(1)
