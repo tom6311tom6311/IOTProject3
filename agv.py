9695af7fcc1695a8c02284cd309a4f6bd7d38185
tomHuang <tom.huang@positivegrid.com>
2018-05-24 17:01:05 +0800

    feat: catture images simultaneously
    Signed-off-by: tomHuang <tom.huang@positivegrid.com>

--- agv.py
+++ agv.py
@@ -1,7 +1,31 @@
 import serial
 import time
+import os
+import shutil
+import subprocess
+import atexit
 
 device_name = '/dev/ttyACM0'
+imgs_dir = 'imgs'
+sub_ps = []
+
+subp_capture_imgs = subprocess.Popen(['python', 'capture_imgs.py', device_name])
+sub_ps.append(subp_capture_imgs)
+
+def on_exit():
+  print('Terminating subprocess...')
+  for p in sub_ps:
+    p.terminate()
+  print('done')
+
+atexit.register(on_exit)
+
+if not os.path.exists(imgs_dir):
+  os.makedirs(imgs_dir)
+else:
+  shutil.rmtree(imgs_dir)
+  os.makedirs(imgs_dir)
+
 
 ser = serial.Serial(device_name, 9600)
 
@@ -13,8 +37,8 @@ while 1:
   if (raw_input):
     ultra_sound_dist = float(raw_input)
     print(ultra_sound_dist)
-  motor_speed[0] = (motor_speed[0] + 1) % 255
-  motor_speed[1] = (motor_speed[1] + 1) % 255
+  motor_speed[0] = (motor_speed[0] + 10) % 255
+  motor_speed[1] = (motor_speed[1] + 10) % 255
   ser.write(''.join([str(speed).zfill(3) for speed in motor_speed]))
   print('Sent: ' + ''.join([str(speed).zfill(3) for speed in motor_speed]))
   # time.sleep(1)
---
+++ capture_imgs.py
@@ -0,0 +1,12 @@
+import sys
+from picamera import PiCamera
+from time import sleep
+
+imgs_dir = sys.argv[1]
+img_idx = 0
+
+camera = PiCamera()
+
+while 1:
+  camera.capture(str(img_idx) + '.jpg')
+  sleep(5)
