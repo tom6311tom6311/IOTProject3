91d230f2a2e4dbd4768a1b34f68b06c5cb255b4e
tomHuang <tom.huang@positivegrid.com>
2018-05-30 16:49:34 +0800

    feat: move along wall
    Signed-off-by: tomHuang <tom.huang@positivegrid.com>

--- agv.py
+++ agv.py
@@ -9,7 +9,7 @@ device_name = '/dev/ttyACM0'
 imgs_dir = 'imgs'
 sub_ps = []
 
-subp_capture_imgs = subprocess.Popen(['python', 'capture_imgs.py', device_name])
+subp_capture_imgs = subprocess.Popen(['python', 'capture_imgs.py', imgs_dir])
 sub_ps.append(subp_capture_imgs)
 
 def on_exit():
@@ -29,15 +29,20 @@ else:
 
 ser = serial.Serial(device_name, 9600)
 
-motor_speed = [0, 0]
+motor_speed = [0.0, 0.0]
 
 while 1:
   raw_input = ser.readline().replace('\r', '').replace('\n', '')
   if (raw_input):
     [front_sonar_dist, left_sonar_dist, angle] = [float(i) for i in raw_input.split(' ')]
-    print(front_sonar_dist)
-  motor_speed[0] = int(front_sonar_dist) % 255
-  motor_speed[1] = int(front_sonar_dist) % 255
-  ser.write(''.join([str(speed).zfill(3) for speed in motor_speed]))
-  print('Sent: ' + ''.join([str(speed).zfill(3) for speed in motor_speed]))
+    print(left_sonar_dist)
+  if (left_sonar_dist > 40 or left_sonar_dist == 0):
+    motor_speed[0] = (motor_speed[0] - 0.005) % 255
+    motor_speed[1] = (motor_speed[1] + 0.005) % 255
+  else:
+    motor_speed[0] = (motor_speed[0] + 0.005) % 255
+    motor_speed[1] = (motor_speed[1] - 0.005) % 255
+
+  ser.write(''.join([str(int(speed)).zfill(3) for speed in motor_speed]))
+  print('Sent: ' + ''.join([str(int(speed)).zfill(3) for speed in motor_speed]))
   # time.sleep(1)
